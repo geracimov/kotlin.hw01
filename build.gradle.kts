3ed1ec1b1c50b310e75096a2e61bd811d8a77b31
geracimov <geracimov@gmail.com>
2020-07-31 01:12:08 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "ru.geracimov.otus.kotlin"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
